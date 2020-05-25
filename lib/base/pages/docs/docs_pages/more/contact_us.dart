aa2f996a2bcc11656d123fe5aef460faf987882c
pulkit <pulkitjims49im@gmail.com>
2020-05-25 11:02:15 +0530

    changed contact us

--- lib/base/pages/docs/docs_pages/more/contact_us.dart
+++ lib/base/pages/docs/docs_pages/more/contact_us.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:liquid/liquid.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactUsPage extends StatelessWidget {
   final _title = "Contact Us";
@@ -31,10 +33,16 @@ class ContactUsPage extends StatelessWidget {
               : _theme.typographyTheme.lead,
         ),
         SizedBox(height: 35.0),
-        LText('\l.h4.bold{Team}\n'),
-        LText(
-            'Liquid is maintained and developed \l.link(href=https://twitter.com/heyrjs){@heyrjs} and \l.link(href=https://twitter.com/heypnd){@heypnd} on GitHub. We at \l.quote.bold{StackOrient Technologies} are actively looking to grow our team and would love to hear from you if '
-            'you\'re exited to develop something cool or are passionate to bring innovations.\n\n'),
+        LText('\l.h4.bold{StackOrient}\n'),
+        LRow(
+          columns: [
+            LColumn.child(
+                child: IconButton(
+                    icon: Icon(FontAwesome.linkedin_square),
+                    onPressed: () => launch(
+                        'https://www.linkedin.com/company/stackorient/?viewAsMember=true')))
+          ],
+        ),
         LText(
             'Liquid is the most advance flutter UI kit for building responsive, beautiful, cross-platform applications. It was created at StackOrient by \l.link(href=https://twitter.com/heypnd){@heypnd} and \l.link(href=https://twitter.com/heyrjs){@heyrjs}. We have made this project to keep responsiveness as one of it'
             's main feature. Along it we make sure to cover all the problems that we have faced being the flutter developers are solved to much of it'
